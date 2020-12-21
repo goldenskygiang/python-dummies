3cb7c1b54762fc19cc05e6e5ae8957d988f035f6
tomnth66 <51469580+tomnth66@users.noreply.github.com>
2020-12-22 01:48:59 +0700

    QuizContent page changed

--- frontend/src/pages/QuizzesContent.js
+++ frontend/src/pages/QuizzesContent.js
@@ -2,34 +2,51 @@ import React, { Component } from "react";
 import "../css/QuizzesContent.css";
 import Layout from "../components/Layout";
 import QuizQuestion from "../components/QuizQuestion.js";
+import QuizContentStatus from "../components/QuizContentStatus.js";
 
 export default class QuizzesContent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      QuizStart: false
+    }
+  }
+
+  StartQuiz(){
+    this.setState({
+      QuizStart: true
+    })
   }
 
   render() {
+    const {QuizStart}  = this.state;
     return (
       <Layout>
-        <div className="QuizzesContentBody">
-          <div className="QuizzesContentBodyHeader">
-            <span id="QCHeader">Python Functions Quiz</span>
-            <span>
-              The quiz contains 13 Questions. Solve 8 correct to pass the test.
-            </span>
-            <span>
-              You will have to read all the given answers and click over the
-              correct answer.
-            </span>
+        <div className="QuizzesContent">
+          <div className = "QuizzesContentBody">
+            {!QuizStart && <div className = "QuizzesMask"></div>}
+
+            <div className="QuizzesContentBodyHeader">
+              <span id="QCHeader">Python Functions Quiz</span>
+              <span>
+                The quiz contains 13 Questions. Solve 8 correct to pass the test.
+              </span>
+              <span>
+                You will have to read all the given answers and click over the
+                correct answer.
+              </span>
+            </div>
+
+            <QuizQuestion></QuizQuestion>
+            <QuizQuestion></QuizQuestion>
+            <QuizQuestion></QuizQuestion>
+
+            <div className="QuizzesSummited">
+              <span>Submit</span>
+            </div>
           </div>
 
-          <QuizQuestion></QuizQuestion>
-          <QuizQuestion></QuizQuestion>
-          <QuizQuestion></QuizQuestion>
-
-          <div className="Summited">
-            <span>Submit</span>
-          </div>
+          <QuizContentStatus StartQuiz = {this.StartQuiz.bind(this)}/>
         </div>
       </Layout>
     );
