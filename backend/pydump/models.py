bb3170f7de5e27de7d2bca9630fe2d6083956fff
Vu Truong Giang <giangthedev@outlook.com.vn>
2020-12-26 00:25:54 +0700

    Added to string methods

--- backend/pydump/models.py
+++ backend/pydump/models.py
@@ -42,11 +42,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.quiz.title} - {self.question_text}"
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     choice_img = models.ImageField()
     is_correct = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.question.question_text} - {self.choice_text}"
 
 class Discussion(models.Model):
     title = models.CharField(max_length=200)
