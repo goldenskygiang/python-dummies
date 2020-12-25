715940b22d4bdb11ccacf4fca5b1c06cbfd40220
newbee1905 <beenewminh@outlook.com>
2020-12-25 23:49:04 +0700

    uodate spinner animation

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -5,7 +5,7 @@ import "../css/Loading.css"
 const Loading = ({ isLoading }) => {
   return (
     <div className={`loading-container ${isLoading ? "" : "finished-loading-container"}`}>
-      <Loader loading={isLoading} color="#2E5288" size={40} />
+      <Loader loading={isLoading} color="#4278c9" size={40} />
     </div>
   )
 };
