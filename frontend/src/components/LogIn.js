8510cb3a4b7aabb0795a06f521220e40d702bf41
newbee1905 <beenewminh@outlook.com>
2020-12-27 02:36:37 +0700

    Fix SignUp and add LogIn

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,28 +1,22 @@
 import React, { useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import "../css/SignUp.css";
+import "./css/SignUp.css";
 
-const SignUp = ({ isOpen }) => {
+const LogIn = ({ isOpen }) => {
   return (
     <Formik
       initialValues={{
-        userName: "",
         email: "",
         password: "",
-        confirmPassword: "",
       }}
       validationSchema={Yup.object().shape({
-        userName: Yup.string().required("User Name is required"),
         email: Yup.string()
           .email("Email is invalid")
           .required("Email is required"),
         password: Yup.string()
           .min(6, "Password must be at least 6 characters")
           .required("Password is required"),
-        confirmPassword: Yup.string()
-          .oneOf([Yup.ref("password"), null], "Passwords must match")
-          .required("Confirm Password is required"),
       })}
       onSubmit={(fields) => {
         alert("SUCCESS!! :-)\n\n" + JSON.stringify(fields, null, 4));
@@ -32,26 +26,6 @@ const SignUp = ({ isOpen }) => {
         <Form className="form">
           <div className="form-container">
             <div className="form-container-inside">
-              <div className="form-group">
-                <Field
-                  name="userName"
-                  type="text"
-                  placeholder=" "
-                  className={
-                    "form-control" +
-                    (errors.userName && touched.userName ? " is-invalid" : "")
-                  }
-                />
-                <div className="invalid-feedback-container">
-                  <ErrorMessage
-                    name="userName"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </div>
-                <div className="underline"></div>
-                <label htmlFor="userName">User Name</label>
-              </div>
               <div className="form-group">
                 <Field
                   name="email"
@@ -92,28 +66,6 @@ const SignUp = ({ isOpen }) => {
                 <div className="underline"></div>
                 <label htmlFor="password">Password</label>
               </div>
-              <div className="form-group">
-                <Field
-                  name="confirmPassword"
-                  type="password"
-                  placeholder=" "
-                  className={
-                    "form-control" +
-                    (errors.confirmPassword && touched.confirmPassword
-                      ? " is-invalid"
-                      : "")
-                  }
-                />
-                <div className="invalid-feedback-container">
-                  <ErrorMessage
-                    name="confirmPassword"
-                    component="div"
-                    className="invalid-feedback"
-                  />
-                </div>
-                <div className="underline"></div>
-                <label htmlFor="confirmPassword">Confirm Password</label>
-              </div>
               <button type="submit">
                 Submit
               </button>
@@ -125,4 +77,4 @@ const SignUp = ({ isOpen }) => {
   );
 };
 
-export default SignUp;
+export default LogIn;
