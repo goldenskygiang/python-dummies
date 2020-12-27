34b121d2ec5540e149d428498be3cb53ec2bbf98
newbee1905 <beenewminh@outlook.com>
2020-12-28 01:48:59 +0700

    Fix bugs in Login and signup

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
+import SignUp from "./SignUp"
+import LogIn from "./LogIn"
 import Footer from "./Footer";
 
 import "../css/Layout.css"
 
 const Layout = ({ children }) => {
+  const [isSignUpOpen, setSignUpOpen] = useState(false);
+  const [isLogInOpen, setLogInOpen] = useState(false);
+  
   return (
     <div className="layout">
-      <Navbar />
+      <SignUp isOpen={isSignUpOpen} setIsOpen={setSignUpOpen} />
+      <LogIn isOpen={isLogInOpen} setIsOpen={setLogInOpen} />
+      <Navbar setSignUpOpen={setSignUpOpen} setLogInOpen={setLogInOpen} />
       <main className="layout--body">{children}</main>
       <Footer />
     </div>
