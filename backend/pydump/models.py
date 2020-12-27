5815cce07ac45eab1ef637479110bc865b0f873f
Vu Truong Giang <giangthedev@outlook.com.vn>
2020-12-27 20:09:01 +0700

    Alter code column

--- backend/pydump/models.py
+++ backend/pydump/models.py
@@ -44,7 +44,7 @@ class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     img = models.ImageField(null=True, blank=True)
     question_text = models.CharField(max_length=200)
-    code = models.TextField()
+    code = models.TextField(null=True, blank=True)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
 
     def __str__(self):
