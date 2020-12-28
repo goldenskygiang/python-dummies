8a73a345c9bf231d10fb8bd459cbb0e0f8845792
newbee1905 <beenewminh@outlook.com>
2020-12-28 23:28:35 +0700

    fix merge issues

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -7,11 +7,6 @@ import LogIn from "./LogIn";
 import "../css/Layout.css";
 
 const Layout = ({ children }) => {
-<<<<<<< HEAD
-  return (
-    <div className="layout">
-      <Navbar />
-=======
   const [isSignUpOpen, setSignUpOpen] = useState(false);
   const [isLogInOpen, setLogInOpen] = useState(false);
 
@@ -20,7 +15,6 @@ const Layout = ({ children }) => {
       <SignUp isOpen={isSignUpOpen} setIsOpen={setSignUpOpen}/>
       <LogIn  isOpen={isLogInOpen} setIsOpen={setLogInOpen}/>
       <Navbar setLogInOpen={setLogInOpen} setSignUpOpen={setSignUpOpen} />
->>>>>>> 2bb2e5022935340d3408c3ad7bf6907c788c37d7
       <main className="layout--body">{children}</main>
       <Footer />
     </div>
